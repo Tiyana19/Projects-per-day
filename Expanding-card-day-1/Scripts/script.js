8bad754688a9483cef755fa0df720a21f77a454d
Tiyana19 <techietiyana@gmail.com>
2023-08-27 01:51:22 +0530

    expanding card project edited

--- Expanding-card-day-1/Scripts/script.js
+++ Expanding-card-day-1/Scripts/script.js
@@ -1,26 +1,19 @@
 const card = document.querySelectorAll(".panel");
- 
 
-card.forEach(panel => {
-    panel.addEventListener("click", () => {
-        removeActicClass();
-        panel.classList.add("active");
-    })
-})
+card.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClass();
+    panel.classList.add("active");
+  });
+});
 
-function removeActicClass() {
-    card.forEach(panel => {
-        panel.classList.remove("active")
-    })
+function removeActiveClass() {
+  card.forEach((panel) => {
+    panel.classList.remove("active");
+  });
 }
 
-
-
-
-
-
-
-                                // For loop method 
+// For loop method
 //  for (let i = 0; i < card.length; i++) {
 // card[i].addEventListener("click", () => {
 //     card[i].classList.toggle("active");
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Projects-per-day
-Making Simple HTML, CSS and JS Projects every day 
+
+Making Simple HTML, CSS and JS Projects every day
+
+## Project 1 : Expanding-cards
+
+In this project image card will be expand when clicking on the certain card.
+
+###Screenshot
+![Preview State](Expanding-card-day-1/design/preview.png)
+
+![Active State](Expanding-card-day-1/design/active-state.png)
